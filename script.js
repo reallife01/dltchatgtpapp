270592e0bd447c3852068d713fe25c017824e709
Lateef Aminu <122064166+reallife01@users.noreply.github.com>
2023-03-19 11:46:56 +0100

    second commit

--- script.js
+++ script.js
@@ -1,71 +1,86 @@
 let questionBox = document.querySelector('#questionBox');
-let apiKey = `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`;
+let apiKey = `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`;
 let responseContainer = document.querySelector('#responseContainer');
 
 
-function askGpt() {
-    const question = questionBox.value;
-    console.log(question);
-    fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-
-        method: "POST",
-        headers: {
-            'Content-Type': "application/json",
-            Authorization: `Bearer ${apiKey}`
-        },
-        body: JSON.stringify({
-            prompt: `${question}`,
-            temperature: 0.7,
-            n: 1,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-            max_tokens: 4000,
-
-        }),
 
 
-    }).then((response) => response.json())
-        .then((resp) => {
-            responseContainer.textContent = resp.choices[0].text;
-            // console.log(resp?.choices[0]?.text);
+// function askGpt() {
+//     const question = questionBox.value;
+//     console.log(question);
+//     fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+
+//         method: "POST",
+//         headers: {
+//             'Content-Type': "application/json",
+//             Authorization: `Bearer ${apiKey}`
+//         },
+//         body: JSON.stringify({
+//             prompt: `${question}`,
+//             temperature: 0.7,
+//             n: 1,
+//             top_p: 1,
+//             frequency_penalty: 0.5,
+//             presence_penalty: 0,
+//             max_tokens: 4000,
+
+//         }),
 
 
-        })
-        .catch((err) => console.log('err on callback', err));
+//     }).then((response) => response.json())
+//         .then((resp) => {
+//             responseContainer.textContent = resp.choices[0].text;
+//             // console.log(resp?.choices[0]?.text);
+
+
+//         })
+//         .catch((err) => console.log('err on callback', err));
+// }
+
+
+const askGpt2 = () => {
+    const searchBtn = document.querySelector('.search');
+    searchBtn.addEventListener('click', async () => {
+        const question = questionBox.value;
+        // console.log('clicked');
+
+        if (question.lenght === 0) {
+            responseContainer.innerHTML = `Input can't be empty`;
+        }
+        else {
+            let fecthResp = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+
+                method: "POST",
+                headers: {
+                    'Content-Type': "application/json",
+                    Authorization: `Bearer ${apiKey}`
+                },
+                body: JSON.stringify({
+                    prompt: `${question}`,
+                    temperature: 0.7,
+                    n: 1,
+                    top_p: 1,
+                    frequency_penalty: 0.5,
+                    presence_penalty: 0,
+                    max_tokens: 4000,
+
+                })
+            })
+
+            try {
+                let parseJson = await fecthResp.json();
+                responseContainer.innerHTML = parseJson?.choices[0]?.text;
+
+
+            }
+            catch (err) {
+                responseContainer.innerHTML = `error catching data`
+            }
+
+        }
+
+
+    })
 }
+askGpt2()
 
-
-async function askGpt2() {
-    const question = questionBox.value;
-    console.log(question);
-    try {
-        let fecthResp = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-
-            method: "POST",
-            headers: {
-                'Content-Type': "application/json",
-                Authorization: `Bearer ${apiKey}`
-            },
-            body: JSON.stringify({
-                prompt: `${question}`,
-                temperature: 0.7,
-                n: 1,
-                top_p: 1,
-                frequency_penalty: 0.5,
-                presence_penalty: 0,
-                max_tokens: 4000,
-
-            }),
-
-        });
-        let parseJson = await fecthResp.json();
-        responseContainer.innerHTML = parseJson?.choices[0]?.text;
-
-
-    }
-    catch (e) {
-        console.log('error on await', e);
-
-    }
-}
